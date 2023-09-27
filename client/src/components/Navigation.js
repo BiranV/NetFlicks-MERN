34331b4c5a23bef774ae88d77ebd9f3c95be1f7b
biran varon <biraniv@gmail.com>
2023-09-27 07:43:58 +0300

    update

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import { onAuthStateChanged, signOut } from "firebase/auth"
 import { auth } from "../firebase"
 
 const Navigation = () => {
   const [authUser, setAuthUser] = useState(null);
-  const navigate = useNavigate();
 
   useEffect(() => {
     const listen = onAuthStateChanged(auth, (user) => {
